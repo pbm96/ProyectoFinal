484ffeaed8a73488893844cc74e96531158e6fc8
Pablo Bonillo <pbonillo96@gmail.com>
2018-04-08 13:08:16 +0200

    includes

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,12 +1,24 @@
 <!-- include del nav y de los enlaces de estilos-->
-@include('templates.header')
-<!-- include de los errores en formularios-->
-@include('templates.errors')
+@extends('templates.main')
+
+@section('pageTitle', 'Home')
+
+@section('estilos')
+    <style></style>
+    @endsection
+
+
+@section('contenido')
+
 @foreach($productos as $producto)
     {{$producto->nombre}}
     <br>
     {{$producto->created_at}}
     @endforeach
 {{ $productos->render() }}
-<!-- include del footer y de los enlaces de scripts-->
-@include('templates.footer')
+@endsection
+
+
+<!-- seccion  de los enlaces de scripts-->
+@section('scripts')
+    @endsection
