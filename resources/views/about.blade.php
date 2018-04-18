995767cbffdd7a38bb06c0706b365a1989673248
Guillermo Priego <gpriego@outlook.com>
2018-04-18 23:14:05 +0200

    Footer,mapa

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,32 +5,68 @@
     </header>
     <div class="about text-justify">
         <h2>F.A.Q</h2>
-        <div class="faq mb-4">
+        <div class="mb-4">
+            <div>
+                <p class="lead">
+                    Pregunta frecuente 1
+                </p>
+                <p>
+                    Respuesta frecuente 1
+                </p>
+            </div>
+            <div>
+                <p class="lead">
+                    Pregunta frecuente 2
+                </p>
+                <p>
+                    Respuesta frecuente 2
+                </p>
+            </div>
+            <div>
+                <p class="lead">
+                    Pregunta frecuente 3
+                </p>
+                <p>
+                    Respuesta frecuente 3
+                </p>
+            </div>
+        </div>
+        <hr class="mb-4">
+        <h2>Cookies</h2>
+        <div class="cookies mb-4">
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. At iste obcaecati molestiae voluptate adipisci deleniti quisquam, exercitationem esse distinctio reiciendis beatae doloremque inventore optio perspiciatis voluptatum dolor, architecto nobis cupiditate?
-                Rerum, aspernatur voluptatem unde consectetur blanditiis in odio, vero deleniti itaque magnam officiis dolor, deserunt dolores quos? Doloremque autem, mollitia sunt delectus iusto labore dicta est sequi dolorem iure corrupti?
-                Corrupti laudantium, cumque modi autem delectus placeat, sit nam repellat harum beatae velit dicta eaque repellendus eius mollitia nostrum atque facilis? Sapiente adipisci inventore possimus quia nostrum et ea voluptatem?
+                Yes. Cookies are small files that a site or its service provider transfers to your computer’s hard drive through your Web browser (if you allow) 
+                that enables the site’s or service provider’s systems to recognize your browser and capture and remember certain information. 
+                For instance, we use cookies to help us remember and process the items in your shopping cart. They are also used to help us understand your preferences based on 
+                previous or current site activity, which enables us to provide you with improved services. 
+                We also use cookies to help us compile aggregate data about site traffic and site interaction so that we can offer better site experiences and tools in the future.
+            </p>
+            We use cookies to: 
+            <ul>
+                <li>Help remember and process the items in the shopping cart.</li>
+                <li>Understand and save user’s preferences for future visits.</li>
+            </ul>
+            <p>
+                We may also use trusted third-party services that track this information on our behalf. You can choose to have your computer warn you each time a cookie is being sent, 
+                or you can choose to turn off all cookies. You do this through your browser settings. Since browser is a little different, look at your browser’s Help Menu to learn 
+                the correct way to modify your cookies. If you turn cookies off, Some of the features that make your site experience more efficient may not function properly.
+                It won’t affect the user’s experience that make your site experience more efficient and may not function properly.
+            </p>
+        </div>
+        <hr class="mb-4">
+        <h2>Copyright</h2>
+        <div class="cookies mb-4">
+            <p>This website and its content is copyright of FakeApop - © FakeApop 2018. All rights reserved. 
+                Any redistribution or reproduction of part or all of the contents in any form is prohibited other than the following:
             </p>
             <ul>
-                <li class=""><strong>fsgfwegwehwerhyer?</strong>
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis earum deserunt corrupti aperiam odit! Adipisci cum sed accusamus cupiditate aliquid exercitationem illum ipsum totam beatae qui. Id tenetur quibusdam aut?
-                    </p>
-                </li>
-                <li class="">
-                    <strong>fsgfwegwehwerhyer?</strong>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, sit. Tempore inventore nesciunt nostrum voluptatum est dicta voluptatibus nobis molestiae maxime suscipit? Sapiente dignissimos suscipit sunt, quod asperiores tempore velit?</p>
-                </li>
-                <li class="">
-                    <strong>fsgfwegwehwerhyer?</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis veniam doloremque dolore voluptates obcaecati eius aut deserunt omnis. Distinctio, eveniet optio! Numquam reiciendis iure accusantium dolores ipsum earum. Explicabo, ea.</p>                    </li>
-                </ul>
-        </div>
-        <hr class="mb-4 bg-dark">
-        <h2>Cookies</h2>
-        <div class="cookies">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint incidunt accusantium fugit, laborum eveniet expedita nostrum exercitationem quaerat atque voluptatibus ex sed tempora ea enim magni quasi. Tempora, labore laborum. <br>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quibusdam quae iure, deleniti placeat nulla reprehenderit nisi nostrum officiis reiciendis, molestias officia iste cumque amet rem odit labore asperiores nihil?</p>
+                <li>you may print or download to a local hard disk extracts for your personal and non-commercial use only</li>
+                <li>you may copy the content to individual third parties for their personal use, but only if you acknowledge the website as the source of the material</li>
+            </ul>
+            <p>
+                    You may not, except with our express written permission, distribute or commercially exploit the content. 
+                    Nor may you transmit it or store it in any other website or other form of electronic retrieval system.
+            </p>
         </div>
     </div>
 @endsection
--- resources/views/productos/ver-producto-individual/index.blade.php
+++ resources/views/productos/ver-producto-individual/index.blade.php
@@ -28,12 +28,12 @@
 
 <div class="container col-sm-8 mt-5">
     <nav>
-        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+        <div class="nav nav-tabs mb-3" id="nav-tab" role="tablist">
             <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#descripcion" role="tab" aria-controls="descripcion" aria-selected="true">Descripcion</a>
             <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#localizacion" role="tab" aria-controls="localizacion" aria-selected="false">Localizacion</a>
         </div>
     </nav>
-    <div class="tab-content" id="nav-tabContent">
+    <div class="tab-content container" id="nav-tabContent">
         <div class="tab-pane fade show active" id="descripcion" role="tabpanel" aria-labelledby="nav-home-tab">{{$producto->descripcion}}</div>
         <div class="tab-pane fade" id="localizacion" role="tabpanel" aria-labelledby="nav-profile-tab">Mapa</div>
     </div>
