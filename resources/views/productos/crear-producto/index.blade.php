713b4601c0cc1cf01f8725e5a382ee83e797630e
Pablo Bonillo <pbonillo96@gmail.com>
2018-04-29 23:10:20 +0200

    crear-estilos

--- resources/views/productos/crear-producto/index.blade.php
+++ resources/views/productos/crear-producto/index.blade.php
@@ -1,27 +1,44 @@
-{!! Form::Open(['route'=>'guardar_producto','method'=>'POST','files'=>true]) !!}
 
-<div class="form-group">
-    {!! Form::label('nombre','Nombre') !!}
-    {!! Form::Text('nombre',null,['class'=>'','required','placeholder'=>'Nombre Producto']) !!}
+@extends('templates.main')
+@section('titulo_pagina', 'Añadir-producto')
+@section('contenido')
+
+{!! Form::Open(['route'=>'guardar_producto','method'=>'POST','files'=>true]) !!}
+<div class="container">
+    <div class="form-row">
+        <div class="form-group col-sm-9">
+            {!! Form::label('nombre','Nombre') !!}
+             {!! Form::Text('nombre',null,['class'=>'form-control','required','placeholder'=>'Nombre Producto']) !!}
+        </div>
+        <div class="form-group col-sm-3">
+            {!! Form::label('precio','Precio') !!}
+            <div class="input-group">
+             {!! Form::number('precio',null,['class'=>'form-control','required','placeholder'=>'Precio Producto']) !!}
+            <div class="input-group-prepend">
+                <div class="input-group-text">€</div>
+            </div>
+            </div>
+        </div>
+    </div>
+<div class="md-form">
+    {!! Form::label('categoria_id','Categoria del producto') !!}
+    {!! Form::select('categoria_id',$categorias,null,['class'=>'form-control','required']) !!}
 </div>
-<div class="form-group">
-    {!! Form::label('precio','Precio') !!}
-    {!! Form::Text('precio',null,['class'=>'','required','placeholder'=>'Precio Producto']) !!}
+<div class="md-form">
+    {!! Form::label('descripcion','Descripcion del producto') !!}
+    {!! Form::textarea('descripcion',null,['class'=>'form-control ','required','placeholder'=>'Descripcion Producto']) !!}
 </div>
-<div class="form-group">
-    {!! Form::label('categoria_id','Categoria') !!}
-    {!! Form::select('categoria_id',$categorias,null,['class'=>'','required']) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('descripcion','Descripcion') !!}
-    {!! Form::textarea('descripcion',null,['class'=>' ','required','placeholder'=>'Descripcion Producto']) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('imagen','Imagen') !!}
+<div class="md-form">
+    {!! Form::label('imagen','Imagenes') !!}
     {!! Form::File('imagen[]',['class'=>'form-control','multiple'=>'multiple']) !!}
 </div>
-<div class="form-group">
-    {!!Form::submit('Agregar articulo',['class'=>'btn btn-primary'])!!}
 
+    <div class="  mt-5">
+         <div class=" row justify-content-end">
+             {!!Form::submit('Añadir Producto',['class'=>'btn btn-primary'])!!}
+         </div>
+    </div>
 </div>
 {!! Form::close() !!}
+    @endsection
+
