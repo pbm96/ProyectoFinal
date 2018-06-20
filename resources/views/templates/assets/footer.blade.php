363954e972d4d97139b8a751c106ee0b5568b73a
Guillermo Priego <gpriego@outlook.com>
2018-06-20 13:13:31 +0200

    Mapa web
    Mapa web

--- resources/views/templates/assets/footer.blade.php
+++ resources/views/templates/assets/footer.blade.php
@@ -2,16 +2,16 @@
     <div class="container">
         <div class="row justify-content-between">
             <div class="col-lg-8 text-white pb-2 text-justify">
-                Ponte en contacto con otros usuarios para intercambiar bienes a través de Fakeapop. <br>
-                Si tienes alguna duda sobre la aplicacion, no dudes en contactar con nosotros mediante un email
-                en la siguiente dirección: <u>Fakeapop@outlook.com</u>
+                Ponte en contacto con otros usuarios para intercambiar bienes a través de Fakeapop. <br> Si tienes alguna
+                duda sobre la aplicacion, no dudes en contactar con nosotros mediante un email en la siguiente dirección:
+                <u>Fakeapop@outlook.com</u>
             </div>
             <div class="col-lg-4">
-                <a href="#" class="text-white">Contacto</a>
+                <a href="{{ route('contact') }}" class="text-white">Contacto</a>
                 <hr class="bg-white">
-                <a href="#" class="text-white">Sobre nosotros</a>
+                <a href="{{ route('about') }}" class="text-white">Sobre nosotros</a>
                 <hr class="bg-white">
-                <a href="#" class="text-white">Mapa web</a>
+                <a href="{{ route('mapa') }}" class="text-white">Mapa web</a>
             </div>
         </div>
         <hr class="bg-white">
