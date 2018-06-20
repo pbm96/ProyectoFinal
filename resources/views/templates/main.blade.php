0ab5e5a048081863c5b324573a12a4600b7f88d2
Guillermo Priego <gpriego@outlook.com>
2018-06-20 13:40:54 +0200

    footer update
    footer update

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -3,22 +3,19 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('titulo_pagina') -Fakeapop</title>
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css"
-          integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9"
-          crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9"
+        crossorigin="anonymous">
     <!-- Bootstrap core CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
     <!-- Material Design Bootstrap -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.0/css/mdb.min.css" rel="stylesheet">
 
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.2/css/bootstrap-slider.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.2/css/bootstrap-slider.min.css">
 
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> @yield('estilos')
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
@@ -26,88 +23,92 @@
 </head>
 
 <body>
-<header class="bg-primary">
+    <header class="bg-primary">
     @include('templates.assets.header')
     @include('cookieConsent::index')
-</header>
+    </header>
 
-<div class="  row justify-content-sm-end mr-4 p-2 font-weight-bold text-center  ">
-    <div class="position-absolute"  id="mensaje_flash">
-        @include('flash::message')
+    <div class="  row justify-content-sm-end mr-4 p-2 font-weight-bold text-center  ">
+        <div class="position-absolute" id="mensaje_flash">
+    @include('flash::message')
+        </div>
     </div>
-</div>
 
-<main class="container ">
-    <div id="sidenav" class="sidenav p-4">
-        <div class="text-right"><a href="#" id="closeIcon" class="close-icon" data-toggle="popover" data-trigger="hover"
-                                   data-placement="right"
-                                   data-content="Cerrar"> &times;</a></div>
-        @guest
+    <main class="container ">
+        <div id="sidenav" class="sidenav p-4">
+            <div class="text-right"><a href="#" id="closeIcon" class="close-icon" data-toggle="popover" data-trigger="hover" data-placement="right"
+                    data-content="Cerrar"> &times;</a></div>
+            @guest
             <h2 class="text-center mb-5">Bienvenido</h2>
             ¿Ya estas registrado?<a href="{{route('login')}}">Entra</a>
             <hr> ¿No tienes una cuenta?<a href="{{route('register')}}">Regístrate</a> @else
-                <div class="row justify-content-sm-center">
-                    @if(auth()->user()->imagen!=null)
-                        <img class="d-flex rounded-circle z-depth-1-half "
-                             src="{{asset('imagenes/perfil/'.auth()->user()->imagen)}}" height="150"
-                             width="150" alt="Avatar"> @else
-                        <img class="d-flex rounded-circle z-depth-1-half "
-                             src="{{asset('imagenes/perfil/user-default.png')}}" height="150" width="150"
-                             alt="Avatar"> @endif
-                </div>
-                <h2 class=" text-center mt-4">{{ Auth::user()->nombre }}</h2>
-                <hr class="mb-5">
-                <section class="lead">
-                    <a class="nav-link text-dark" href="{{ route('ver_productos_usuario',auth()->user()->id)}}"> <span
+            <div class="row justify-content-sm-center">
+                @if(auth()->user()->imagen!=null)
+                <img class="d-flex rounded-circle z-depth-1-half " src="{{asset('imagenes/perfil/'.auth()->user()->imagen)}}" height="150"
+                    width="150" alt="Avatar"> @else
+                <img class="d-flex rounded-circle z-depth-1-half " src="{{asset('imagenes/perfil/user-default.png')}}" height="150" width="150"
+                    alt="Avatar"> @endif
+            </div>
+            <h2 class=" text-center mt-4">{{ Auth::user()->nombre }}</h2>
+            <hr class="mb-5">
+            <section class="lead">
+                <a class="nav-link text-dark" href="{{ route('ver_productos_usuario',auth()->user()->id)}}"> <span
                                 class="fa fa-clipboard-list text-primary"></span> Mis Productos</a>
-                    <a class="nav-link text-dark" href="{{ route('mis_mensajes',auth()->user()->id)}}"> <span
+                <a class="nav-link text-dark" href="{{ route('mis_mensajes',auth()->user()->id)}}"> <span
                                 class="fas fa-comments text-primary"></span> Mis Mensajes</a>
-                    <a class="nav-link text-dark"
-                       href="{{ route('ver_productos_usuario_favoritos',auth()->user()->id)}}"> <span
+                <a class="nav-link text-dark" href="{{ route('ver_productos_usuario_favoritos',auth()->user()->id)}}"> <span
                                 class="fa fa-heart text-danger"></span> Mis Favoritos</a>
-                    <a class="nav-link text-dark" href="{{ route('administrar_perfil',auth()->user()->id)}}"> <span
+                <a class="nav-link text-dark" href="{{ route('administrar_perfil',auth()->user()->id)}}"> <span
                                 class="fa fa-edit text-success"></span> Editar Perfil</a>
 
-                    <a href="{{ route('logout')}}" class="nav-link text-dark" onclick="event.preventDefault();
+                <a href="{{ route('logout')}}" class="nav-link text-dark" onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();"> <span class="fas fa-sign-out-alt"></span> Log
                         Out
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             {{ csrf_field() }}
                         </form>
                     </a>
-                </section>
-                @endguest
-    </div>
-    <div class="wrapper mt-2 ">
-        <div class="content">@yield('contenido')</div>
-        <div class="fixed-action-btn smooth-scroll" style="bottom: 45px; right: 24px;" data-toggle="popover"
-             data-trigger="hover"
-             data-placement="right" data-content="Crear producto">
-            <a href="{{ route('crear_producto') }}" class="btn-floating btn-large bg-info waves-effect waves-light">
+            </section>
+            @endguest
+        </div>
+        <div class="wrapper mt-2 ">
+            <div class="content">@yield('contenido')</div>
+            <div class="fixed-action-btn smooth-scroll" style="bottom: 45px; right: 24px;" data-toggle="popover" data-trigger="hover"
+                data-placement="right" data-content="Crear producto">
+                <a href="{{ route('crear_producto') }}" class="btn-floating btn-large bg-info waves-effect waves-light">
                 <i class="fa fa-plus"></i>
             </a>
+            </div>
         </div>
-    </div>
+        <div class="push"></div>
+    </main>
+    @include('templates.assets.footer')
 
-</main>
-@include('templates.assets.footer')
+    <!-- JQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.0/js/mdb.min.js"></script>
 
-<!-- JQuery -->
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!-- Bootstrap tooltips -->
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
-<!-- Bootstrap core JavaScript -->
-<script type="text/javascript"
-        src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<!-- MDB core JavaScript -->
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.0/js/mdb.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.2/bootstrap-slider.min.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.2/bootstrap-slider.min.js"></script>
+    <script>
+        @auth()
 
-<script>
-    @auth()
     $(document).ready(function ()
     {
+        $(window).resize(function(){
+			    var footerHeight = $('.footer').outerHeight();
+			    var stickFooterPush = $('.push').height(footerHeight);
+		
+    			$('main').css({'marginBottom':'-' + footerHeight + 'px'});
+		    });
+		
+    		$(window).resize();
+            
         var route= "{{route('notificaciones')}}";
 
         $.ajax({
@@ -174,9 +175,10 @@
     }else{
         $("#mensaje_flash").remove();
     }
-</script>
 
-@yield('scripts')
+    </script>
+
+    @yield('scripts')
 
 
 </body>
